cea76ee1c53bf5fe5f1ba134f22e4e3d29aafde0
Eugen Ciur <eugen@django-lessons.com>
2020-01-15 13:29:02 +0100

    fixing failing tests. Added some simple test data

--- test/test_mime.py
+++ test/test_mime.py
@@ -3,9 +3,10 @@ from pathlib import Path
 import unittest
 from pmworker import mime
 
-
-# points to digilette.testing folder
-BASE_DIR = Path(__file__).parent.parent
+DATA_DIR = os.path.join(
+    Path(__file__).parent,
+    'data'
+)
 
 
 class TestConvert(unittest.TestCase):
@@ -13,10 +14,8 @@ class TestConvert(unittest.TestCase):
     def test_basic_command_call(self):
 
         file_path = os.path.join(
-            BASE_DIR,
-            "data",
-            "files",
-            "kyuss.pdf"
+            DATA_DIR,
+            "input.de.pdf"
         )
         mime_type = mime.Mime(filepath=file_path)
         self.assertTrue(
