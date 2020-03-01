aa06d04cfe7b75fbc322498fa7ec7242a908558c
Eugen Ciur <eugen@django-lessons.com>
2020-03-01 08:13:33 +0100

    now with tests

--- test/test_pdftk.py
+++ test/test_pdftk.py
@@ -1,6 +1,9 @@
 import unittest
 from pathlib import Path
-from pmworker.pdftk import cat_ranges_for_delete
+from pmworker.pdftk import (
+    cat_ranges_for_delete,
+    cat_ranges_for_reorder
+)
 
 
 test_dir = Path(__file__).parent
@@ -9,6 +12,50 @@ abs_path_input_pdf = test_data_dir / Path("input.de.pdf")
 
 
 class TestPDFTk(unittest.TestCase):
+
+    def test_cat_ranger_for_reorder(self):
+        # swap first and second pages
+        result = cat_ranges_for_reorder(
+            page_count=4,
+            new_order=[
+                {'page_num': 2, 'page_order': 1},
+                {'page_num': 1, 'page_order': 2},
+                {'page_num': 3, 'page_order': 3},
+                {'page_num': 4, 'page_order': 4}
+            ]
+        )
+        self.assertEqual(
+            result,
+            [2, 1, 3, 4],
+        )
+
+        # swap first and last pages
+        result = cat_ranges_for_reorder(
+            page_count=4,
+            new_order=[
+                {'page_num': 4, 'page_order': 1},
+                {'page_num': 2, 'page_order': 2},
+                {'page_num': 3, 'page_order': 3},
+                {'page_num': 1, 'page_order': 4}
+            ]
+        )
+        self.assertEqual(
+            result,
+            [4, 2, 3, 1],
+        )
+
+        # swap pages in two pages document
+        result = cat_ranges_for_reorder(
+            page_count=2,
+            new_order=[
+                {'page_num': 2, 'page_order': 1},
+                {'page_num': 1, 'page_order': 2},
+            ]
+        )
+        self.assertEqual(
+            result,
+            [2, 1],
+        )
 
     def test_cat_ranges_for_delete(self):
         result = cat_ranges_for_delete(
