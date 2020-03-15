cbec10bce66edcf2856d631fe71b1ccebc3987cb
Eugen Ciur <eugen@django-lessons.com>
2020-03-15 18:53:14 +0100

    pretty good code for worker's page_pages_into_existing_doc

--- pmworker/pdftk.py
+++ pmworker/pdftk.py
@@ -101,12 +101,127 @@ def make_sure_path_exists(filepath):
     )
 
 
+def split_ranges(total, after=False, before=False):
+    """
+    Given a range 1, 2, ..., total (page numbers of a doc).
+    Split it in two lists.
+    Example:
+    Input: total = 9, after=1, before=False
+    Output: list1 = [1]; list2 = [2, 3, 4, ..., 9].
+
+    Input: total = 9; after=False, before=1
+    Output: list1 = [], list2 = [1, 2, 3, 4, ..., 9]
+
+    Input: total = 5; after=4; before=False
+    Output: list1 = [1, 2, 3, 4] list2 = [5]
+
+    Input: total = 5; after=False; before=False;
+    Output: list1 = [1, 2, 3, 4, 5], list2 = []
+    (it means, by default, all pages are inserted at the end of the doc)
+    """
+    if after and not before:
+        if not isinstance(after, int):
+            raise ValueError(
+                "argument 'after' is supposed to be an int"
+            )
+        list1 = list(range(1, after + 1))
+        list2 = list(range(after + 1, total))
+        return list1, list2
+
+    if not after and before:
+        if not isinstance(before, int):
+            raise ValueError(
+                "argument 'before' is supposed to be an int"
+            )
+        list1 = list(range(1, before))
+        list2 = list(range(before, total))
+        return list1, list2
+
+    return list(range(1, total + 1))
+
+
+def paste_pages_into_existing_doc(
+    dest_doc_ep,
+    src_doc_ep_list,
+    after_page_number=False,
+    before_page_number=False
+):
+    page_count = get_pagecount(dest_doc_ep.url())
+    list1, list2 = split_ranges(
+        total=page_count,
+        after=after_page_number,
+        before=before_page_number
+    )
+    # notice missing A
+    # Letter A is assignent to current folder and
+    # pages from list1 and list2
+    letters = "BCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letters_2_doc_map = []
+    letters_pages = []
+    letters_pages_before = []
+    letters_pages_after = []
+
+    letters_2_doc_map.append(
+        f"A={dest_doc_ep.url()}"
+    )
+
+    for idx in range(0, len(src_doc_ep_list)):
+        letter = letters[idx]
+        doc_ep = src_doc_ep_list[idx]['doc_ep']
+        pages = src_doc_ep_list[idx]['page_nums']
+
+        letters_2_doc_map.append(
+            f"{letter}={doc_ep.url()}"
+        )
+        for p in pages:
+            letters_pages.append(
+                f"{letter}{p}"
+            )
+
+    dest_doc_ep.inc_version()
+
+    for p in list1:
+        letters_pages_before.append(
+            f"A{p}"
+        )
+
+    for p in list2:
+        letters_pages_after.append(
+            f"A{p}"
+        )
+
+    cmd = [
+        "pdftk",
+    ]
+    # add A=doc1_path, B=doc2_path
+    cmd.extend(letters_2_doc_map)
+
+    cmd.append("cat")
+
+    # existing doc pages (may be empty)
+    cmd.extend(letters_pages_before)
+    # newly inserted pages
+    cmd.extend(letters_pages)
+    # existing doc pages (may be empty)
+    cmd.extend(letters_pages_after)
+
+    cmd.append("output")
+
+    make_sure_path_exists(dest_doc_ep.url())
+
+    cmd.append(dest_doc_ep.url())
+
+    run(cmd)
+
+    return dest_doc_ep.version
+
+
 def paste_pages(
     dest_doc_ep,
     src_doc_ep_list,
     dest_doc_is_new=True,
-    after_page_number=-1,
-    before_page_number=-1
+    after_page_number=False,
+    before_page_number=False
 ):
     """
     dest_doc_ep = endpoint of the doc where newly created
@@ -142,6 +257,14 @@ def paste_pages(
     If both before_page_number and after_page_number are < 0 - just paste
     pages at the end of the document.
     """
+
+    if not dest_doc_is_new:
+        return paste_pages_into_existing_doc(
+            dest_doc_ep=dest_doc_ep,
+            src_doc_ep_list=src_doc_ep_list,
+            after_page_number=after_page_number,
+            before_page_number=before_page_number
+        )
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     letters_2_doc_map = []
     letters_pages = []
